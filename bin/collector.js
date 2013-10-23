251c388caafeb0f6bb5003dac5cc310c4a9d059e
Matt Zukowski <mzukowski@adknowledge.com>
2013-10-23 12:45:29 -0500

    disabled cluster mode -- will let pm2 handle this instead

--- bin/collector.js
+++ bin/collector.js
@@ -1,17 +1,9 @@
-var cluster = require('cluster');
-
 var options = require("./collector-config"),
   cube = require("../"),
   server = cube.server(options);
 
-if (cluster.isMaster) {
-  for (var i = 0; i < (options.workers || 1); i++) {
-    cluster.fork();
-  }
-} else {
-  server.register = function(db, endpoints) {
-    cube.collector.register(db, endpoints);
-  };
+server.register = function(db, endpoints) {
+  cube.collector.register(db, endpoints);
+};
 
-  server.start();
-}
+server.start();
--- bin/evaluator.js
+++ bin/evaluator.js
@@ -1,17 +1,8 @@
-var cluster = require('cluster');
-
 var options = require("./evaluator-config"),
   cube = require("../"),
   server = cube.server(options);
 
-if (cluster.isMaster) {
-  for (var i = 0; i < (options.workers || 1); i++) {
-    cluster.fork();
-  }
-} else {
-  server.register = function(db, endpoints) {
-    cube.evaluator.register(db, endpoints);
-  };
+server.register = function(db, endpoints) {
+  cube.evaluator.register(db, endpoints);
+};
 
-  server.start();
-}
